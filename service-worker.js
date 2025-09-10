d4608974b318fd7cf511a79b968e64389657442c
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-09-10 21:44:13 +0700

    add README.md

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("pwa-demo-cache").then((cache) => {
+      return cache.addAll(["/index.html", "/manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
